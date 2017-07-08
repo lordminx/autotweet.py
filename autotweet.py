e92d6c1aeb34148a6c4a7f20d847c724b5eac3e5
lordminx <admin@story-games.at>
2017-07-08 22:01:45 +0200

    Initial Commit

---
+++ autotweet.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""\
+Usage:
+    autotweet [-hc CONFIG] [ tweet | toot | all ]
+
+    -h --help   Print out this help message.
+    -c FILE     Specify config file. [default: ./autotweet.json]
+    tweet       Post to Twitter.
+    toot        Post to Mastodon.
+    all         Post to all configured sites.
+
+"""
+
+
+import sys
+import json
+import feedparser
+import docopt
+
+from twitter import *
+from mastodon import Mastodon
+from bs4 import BeautifulSoup
+
+import requests as r
+
+
+def loadjson(conf):
+    """ Load json-formatted config file."""
+    with open(conf) as f:
+        return json.load(f)
+
+def getfeed(feed):
+    """ Load and parse the given rss/atom feed."""
+
+    fp = feedparser.parse(feed)
+
+    title = fp["entries"][0]["title"]
+    link = fp["entries"][0]["link"]
+    content = fp["entries"][0]["summary"]
+
+    print("Feed fetched and parsed.")
+    return title, link, content
+
+def hasimage(post):
+    """ Check post for image links."""
+
+    soup = BeautifulSoup(content, "html.parser")
+
+    if soup.img:
+        src = soup.img["src"]
+        print("Image link found.")
+        return src
+    else:
+        return None
+
+def getimage(link):
+    """ Download the image."""
+
+    res = r.get(link)
+    if res.status_code == 200:
+        img = res.content
+        mime = res.headers["Content-Type"]
+        print("Image downloaded.")
+
+    return img, mime
+
+def makepost(title, link, content=None):
+    """ Build tweet/toot text. """
+    return "Blogpost: \"{}\" {}".format(title, link)
+
+def twitterconf(config):
+    """Extract Twitter connection data from config dict."""
+    tc = config["Twitter"]
+    keys = ["OAUTH_TOKEN",
+            "OAUTH_SECRET",
+            "CONSUMER_KEY",
+            "CONSUMER_SECRET"]
+
+    return tuple([tc[key] for key in keys])
+
+
+def tweet(post, conf, img=None):
+    """ Post to Twitter."""
+
+    # extract twitter confg values
+    conf = twitterconf(config)
+
+    # Create Twitter connection
+    t = Twitter(auth=OAuth(*conf))
+
+    # make twitter post
+    if img:
+        print("Uploading Image to Twitter...")
+        t_upload = Twitter(domain='upload.twitter.com', auth=OAuth(*conf))
+        img_id = t_upload.media.upload(media=img)["media_id_string"]
+        print("Done.")
+
+    print("Tweeting...")
+
+    if img:
+        t.statuses.update(status=post, media_ids=",".join([img_id]))
+    else:
+        t.statuses.update(status=post)
+
+    print("Done.")
+
+def mastodonconf(config):
+    """Extract Mastodon connection data from config dict."""
+    conf = config["mastodon"]
+    keys = ["clientfile", "userfile"]
+
+    return tuple([conf[key] for key in keys])
+
+def toot(post, config, img=None, mime=None):
+    """Post to Mastodon."""
+
+    clientfile, userfile = mastodonconf(config)
+
+    # Create Mastodon Connection
+    m = Mastodon(client_id=clientfile, access_token=userfile)
+
+    # make mastodon post
+    if img:
+        print("Uploading Image to Mastodon...")
+        media = m.media_post(media_file=img, mime_type=mime)
+        print("Done.")
+
+    print("Tooting...")
+
+    if img:
+        m.status_post(post, media_ids=[media["id"]])
+    else:
+        m.toot(post)
+
+    print("Done.")
+
+
+if __name__ == "__main__":
+    options = docopt.docopt(__doc__)
+
+    try:
+        config = loadjson(options["-c"])
+    except OSError as e:
+        print(e)
+        sys.exit()
+
+    try:
+
+        feed = config["feed"]
+        title, link, content = getfeed(feed)
+        img = hasimage(content)
+        if img:
+            img, mime = getimage(img)
+        else:
+            img = None
+            mime = None
+        post = makepost(title, link)
+
+    except IndexError:
+        print("Feed could not be loaded.")
+        sys.exit()
+
+    if options["tweet"]:
+        tweet(post, config, img)
+
+    if options["toot"]:
+        toot(post, config, img, mime)
+
+    if options["all"]:
+        toot(post, config, img, mime)
+        tweet(post, config, img)
